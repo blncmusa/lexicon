ac56e563bfc54d163af0d1dabf140dc5f973f7c3
Mustafa <musa.saalih11@gmail.com>
2023-02-06 17:54:45 +0000

    added: template literals that display fetch results; catch to display error for words not contained in dictionary, fixed list style type, nested all the defintions in their respective meanings.

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ toggle.addEventListener("change", function() {
     logo.style.color = "white"
     select.style.color = "white"
     select.style.backgroundColor = "#212121"
-    
   } else {
     document.body.style.backgroundColor = "white";
     document.body.style.color = "black";
@@ -25,6 +24,11 @@ toggle.addEventListener("change", function() {
 
 const searchInput = document.getElementById("search-input");
 const searchButton = document.getElementById("search-button");
+const word = document.querySelector(".word")
+const wordSection = document.querySelector(".word-section")
+const phonetic = document.querySelector(".pronunciation")
+const definitionContainer = document.querySelector('.generated-definitions')
+const meaningBody = document.querySelector('.meaning-body')
 
 searchInput.addEventListener("keypress", event => {
     if (event.key === "Enter") {
@@ -38,5 +42,56 @@ searchInput.addEventListener("keypress", event => {
     console.log(searchValue)
     searchInput.value = ""
     // do something with the search value (e.g. send it to the server to get results)
-    fetch()
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchValue}`)
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    console.log(data[0])
+    const meaning = data[0].meanings.map(meaning => {
+      const definition = meaning.definitions.map(definition => {
+        wordSection.innerHTML = `<section class="word-section">
+        <div class="word-section-left">
+            <h1 class="word">${data[0].word}</h1>
+            <p class="pronunciation">/${data[0].phonetics[1].text}/</p>
+        </div>
+        <div class="word-section-right">
+            <button class="play-btn"><i class="fa-solid fa-play"></i></button>
+        </div>
+    </section>`
+        return `⁠<li>${definition.definition}</li>`;
+      }).join("");
+      return `
+        <article class="definition">
+          <div class="definition-header">
+            <p class="part-of-speech">${meaning.partOfSpeech}</p> 
+            <hr id="hr"> 
+          </div>
+          <section>
+            <h1 class="meaning-header">Meaning</h1>
+              <div class="meaning-body">
+                <ul>
+                  ${definition}
+                </ul>
+              </div>
+            </section>
+          </article>
+        `;
+    }).join("");
+    definitionContainer.innerHTML = "";
+    definitionContainer.insertAdjacentHTML("beforeend", meaning);
+  }).catch(error => {
+    wordSection.innerHTML = ""
+    console.error(error);
+    definitionContainer.innerHTML = `<div class="error-msg"><h1>Oops... I can't find "${searchValue}" in this dictionary!!</h1><p id="error-img"><i class="fa-solid fa-file-circle-exclamation"></i></p></div>`
   });
+  });
+
+  // Font Selector 
+
+  const fontDropDown = document.getElementById("select");
+  const body = document.querySelector("body")
+
+  fontDropDown.addEventListener("change", function(event){
+    body.style.fontFamily = event.target.value;
+  })
